41f49e1f7002ba6c553def90eb4416b02fd78b82
Shravastee Thakur <shravastee@gmail.com>
2024-12-23 12:31:48 +0530

    last commit

--- Frontend/src/Context/PasswordContext.jsx
+++ Frontend/src/Context/PasswordContext.jsx
@@ -1,13 +1,80 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
 
 const PasswordContext = createContext();
 
 export const usePasswordContext = () => useContext(PasswordContext);
 
 export const PasswordProvider = ({ children }) => {
+  // state for adding new password
+  const [newPassword, setNewPassword] = useState({
+    title: "",
+    password: "",
+  });
+
+  // function for adding new password
+  const [allPassword, setAllPassword] = useState([]);
+
+  // post password
+  const PostPassword = async (data) => {
+    console.log(data);
+
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/pwm/api/password/postpassword",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token"),
+          },
+        }
+      );
+
+      let postData = res.data;
+
+      if (postData?.success) {
+        let mainData = postData?.data;
+        setAllPassword([...allPassword, mainData]);
+        setNewPassword({ title: "", password: "" });
+      } else {
+        console.log(postData?.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // get password
+  const GetPassword = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/pwm/api/password/getpassword",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token"),
+          },
+        }
+      );
+
+      let getData = res?.data;
+      console.log(getData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <PasswordContext.Provider value={"This is context data"}>
+    <PasswordContext.Provider
+      value={{
+        PostPassword,
+        GetPassword,
+        newPassword,
+        setNewPassword,
+        allPassword,
+      }}
+    >
       {children}
     </PasswordContext.Provider>
   );
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PasswordItem from "../Components/PasswordItem";
 import { usePasswordContext } from "../Context/PasswordContext";
 
 const Home = () => {
-  console.log(usePasswordContext());
+  const {
+    PostPassword,
+    GetPassword,
+    allPassword,
+    newPassword,
+    setNewPassword,
+  } = usePasswordContext();
+
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    PostPassword(newPassword);
+  };
+
+  useEffect(() => {
+    GetPassword();
+  }, []);
 
   return (
     <div className="min-h-screen w-full bg-slate-200 p-5">
@@ -16,13 +31,20 @@ const Home = () => {
       </div>
 
       <section className="flex justify-center items-center mt-10 p-5">
-        <form className="w-full sm:w-1/2 lg:w-1/3 mx-auto flex flex-col gap-2 bg-sky-300 p-5 rounded-lg">
+        <form
+          onSubmit={HandleSubmit}
+          className="w-full sm:w-1/2 lg:w-1/3 mx-auto flex flex-col gap-2 bg-sky-300 p-5 rounded-lg"
+        >
           <label className="text-balance font-semibold">Title</label>
           <input
             className="rounded-md p-2 outline-none focus:ring-2 focus:ring-teal-600"
             type="text"
             placeholder="Enter Your title"
             name="title"
+            value={newPassword?.title}
+            onChange={(e) =>
+              setNewPassword({ ...newPassword, title: e.target.value })
+            }
           />
 
           <label className="text-balance font-semibold">Password</label>
@@ -32,6 +54,10 @@ const Home = () => {
             type="password"
             placeholder="Enter Your Password"
             name="password"
+            value={newPassword?.password}
+            onChange={(e) =>
+              setNewPassword({ ...newPassword, password: e.target.value })
+            }
           />
 
           <button className="bg-blue-700 p-2 rounded-md text-white font-bold w-1/2  mx-auto mt-3 hover:bg-blue-600 cursor-pointer">
@@ -42,11 +68,11 @@ const Home = () => {
 
       {/* password section */}
       <section className="mt-5 grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
-        <PasswordItem />
-        <PasswordItem />
-        <PasswordItem />
-        <PasswordItem />
-        <PasswordItem />
+        {allPassword &&
+          allPassword?.length > 0 &&
+          allPassword.map((value) => {
+            <PasswordItem key={value?._id} value={value} />;
+          })}
       </section>
     </div>
   );
